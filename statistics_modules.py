cc11f6aeab6964eb7e8b0644cdb2014bbeb968a5
Jetske van Kampen <j.van.kampen@st.hanze.nl>
2024-04-03 13:57:52 +0200

    new

--- statistics_modules.py
+++ statistics_modules.py
@@ -5,6 +5,105 @@ import matplotlib.pyplot as plt
 from scipy.stats import anderson
 from scipy.stats import norm
 
+def DS_Q_Q_Plot_test(y, est = 'robust', **kwargs):
+    """
+    *
+    Function DS_Q_Q_Plot(y, est = 'robust', **kwargs)
+    
+       This function makes a normal quantile-quantile plot (Q-Q-plot), also known
+       as a probability plot, to visually check whether data follow a normal distribution.
+    
+    Requires:            - 
+    
+    Arguments:
+      y                  data array
+      est                Estimation method for normal parameters mu and sigma:
+                         either 'robust' (default), or 'ML' (Maximum Likelihood),
+                         or 'preset' (given values)
+      N.B. If est='preset' than the *optional* parameters mu, sigma must be provided:
+      mu                 preset value of mu
+      sigma              preset value of sigma
+      
+    Returns:
+      Estimated mu, sigma, n, and expected number of datapoints outside CI in Q-Q-plot.
+      Q-Q-plot
+      
+    Author:            M.E.F. Apol
+    Date:              2020-01-06, revision 2022-08-30
+    """
+    
+    import numpy as np
+    from scipy.stats import iqr
+    import matplotlib.pyplot as plt
+    from scipy.stats import norm
+    
+    # First, get the optional arguments mu and sigma:
+    mu_0 = kwargs.get('mu', None)
+    sigma_0 = kwargs.get('sigma', None)
+    
+    n = len(y)
+    
+    # Calculate order statistic:
+    y_os = np.sort(y)
+  
+    # Estimates of mu and sigma:
+    # ML estimates:
+    mu_ML = np.mean(y)
+    sigma2_ML = np.var(y)
+    sigma_ML = np.std(y) # biased estimate
+    s2 = np.var(y, ddof=1)
+    s = np.std(y, ddof=1) # unbiased estimate
+    # Robust estimates:
+    mu_R = np.median(y)
+    sigma_R = iqr(y)/1.349
+
+    # Assign values of mu and sigma for z-transform:
+    if est == 'ML':
+        mu, sigma = mu_ML, s
+    elif est == 'robust':
+        mu, sigma = mu_R, sigma_R
+    elif est == 'preset':
+        mu, sigma = mu_0, sigma_0
+    else:
+        print('Wrong estimation method chosen!')
+        return()
+        
+    
+    # Expected number of deviations (95% confidence level):
+    n_dev = np.round(0.05*n)
+    
+         
+    # Perform z-transform: sample quantiles z.i
+    z_i = (y_os - mu)/sigma
+
+    # Calculate cumulative probabilities p.i:
+    i = np.array(range(n)) + 1
+    p_i = (i - 0.5)/n
+
+    # Calculate theoretical quantiles z.(i):
+    
+    z_th = norm.ppf(p_i, 0, 1)
+
+    # Calculate SE or theoretical quantiles:
+    SE_z_th = (1/norm.pdf(z_th, 0, 1)) * np.sqrt((p_i * (1 - p_i)) / n)
+
+    # Calculate 95% CI of diagonal line:
+    CI_upper = z_th + 1.96 * SE_z_th
+    CI_lower = z_th - 1.96 * SE_z_th
+
+    # Make Q-Q plot:
+    plt.plot(z_th, z_i, 'o', color='k', label='experimental data')
+    plt.plot(z_th, z_th, '--', color='r', label='normal line')
+    plt.plot(z_th, CI_upper, '--', color='b', label='95% CI')
+    plt.plot(z_th, CI_lower, '--', color='b')
+
+    stats_text = f'Estimation method: {est}\nn = {n}, mu = {mu:.4g}, sigma = {sigma:.4g}\nExpected number of data outside CI: {n_dev:.0f}'
+    plt.text(1.05, 0.95, stats_text, transform = plt.gca(). transAxes, fontsize=9, verticalalignment='top')
+    plt.xlabel('Theoretical quantiles, $z_{(i)}$')
+    plt.ylabel('Sample quantiles, $z_i$')
+    plt.title('Q-Q plot (' + est + ')')
+    plt.legend(loc='best')
+    plt.show()
 def DS_Q_Q_Plot(y, est = 'robust', **kwargs):
     """
     *
